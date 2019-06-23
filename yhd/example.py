b2955a26085fac6c66dcc8123e751e4b82f992b6
jiabin <darcy@jiabins-Mac-mini.local>
2019-06-24 00:45:41 +0800

    :new: yhd demo finished

--- yhd/example.py
+++ yhd/example.py
@@ -1,46 +1,36 @@
-import requests
 import time
+import json
+import re
 
-base_url = 'https://search.yhd.com/searchPage/c0-0/mbname%E5%BE%AE%E8%BD%AF%EF%BC%88Microsoft%EF%BC%89-b/a-s1-v4-p1-price-d0-f0b-m1-rt0-pid-mid0-color-size-ksurface+book/?isGetMoreProducts=1&moreProductsDefaultTemplate=0&isLargeImg=0&moreProductsFashionCateType=2&nextAdIndex=0&nextImageAdIndex=0&adProductIdListStr=&fashionCateType=2&firstPgAdSize=0&needMispellKw=&onlySearchKeyword=1&_='
+import requests
+from urllib.parse import urlencode
 
-timestamp = int(time.time() * 1000)
-url = base_url + str(timestamp)
+base_url = 'https://search.yhd.com/searchPage/c0-0/mbnameApple-b/a-s1-v4-p1-price-d0-f0b-m1-rt0-pid-mid0-color-size-kmacbook/?'
+params = {
+    "isGetMoreProducts": "1",
+    "moreProductsDefaultTemplate": "0",
+    "isLargeImg": "0",
+    "moreProductsFashionCateType": "2",
+    "nextAdIndex": "0",
+    "nextImageAdIndex": "0",
+    "adProductIdListStr": "",
+    "fashionCateType": "2",
+    "firstPgAdSize": "0",
+    "needMispellKw": "",
+    "onlySearchKeyword": "1",
+    "_": int(time.time() * 1000),
+}
+url = base_url + urlencode(params)
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3818.0 Safari/537.36 Edg/77.0.189.3',
     'Referer': 'https://www.yhd.com/',
-
 }
 
 response = requests.get(url, headers=headers)
-# with open('example.html', 'w', encoding='utf-8') as f:
-#     f.write(response.content.decode('utf-8'))
-print(response.content.decode('utf-8'))
-
-
-# callback: jQuery111307046293252049012_1561023575257
-# isGetMoreProducts: 1
-# moreProductsDefaultTemplate: 0
-# isLargeImg: 0
-# moreProductsFashionCateType: 2
-# nextAdIndex: 0
-# nextImageAdIndex: 0
-# adProductIdListStr: 
-# fashionCateType: 2
-# firstPgAdSize: 0
-# needMispellKw: 
-# onlySearchKeyword: 1
-# _: 1561023575259
-
-# callback: jQuery11130007428571518508065_1561023686812
-# isGetMoreProducts: 1
-# moreProductsDefaultTemplate: 0
-# isLargeImg: 0
-# moreProductsFashionCateType: 2
-# nextAdIndex: 0
-# nextImageAdIndex: 0
-# adProductIdListStr: 
-# fashionCateType: 2
-# firstPgAdSize: 0
-# needMispellKw: 
-# onlySearchKeyword: 1
-# _: 1561023686814
+content = response.json().get('value')
+pattern = re.compile('<b>¥</b>(.*?)\n\t</em>.*?title=(.*?) singleFreeFlag', re.S)
+items = re.findall(pattern, content)
+for item in items:
+    print(item) # price + details
+    
+print('finished')
